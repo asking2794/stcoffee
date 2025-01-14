45c5d8526c15816934d1acd0c6823d69d03a41c5
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-01-14 23:35:28 +0700

    Create body.js

---
+++ body.js
@@ -0,0 +1,8 @@
+ atOptions = {
+		'key' : '7b8bc179ccc7816266dd1427c3863bb0',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/7b8bc179ccc7816266dd1427c3863bb0/invoke.js"></scr' + 'ipt>');
