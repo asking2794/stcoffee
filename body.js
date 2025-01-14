1c65e5aac2cb1e6c9bff8939b43b3fc4dda3ce97
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-01-14 23:36:17 +0700

    Update body.js

--- body.js
+++ body.js
@@ -1,8 +1,18 @@
- atOptions = {
-		'key' : '7b8bc179ccc7816266dd1427c3863bb0',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+atOptions = {
+    'key' : '7b8bc179ccc7816266dd1427c3863bb0',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+
 document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/7b8bc179ccc7816266dd1427c3863bb0/invoke.js"></scr' + 'ipt>');
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
