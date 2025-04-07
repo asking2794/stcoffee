3c311ae344099ceedda535390a9b576b0fec550b
asking2794 <146243355+asking2794@users.noreply.github.com>
2025-04-07 20:27:40 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -7,4 +7,4 @@
 	};
 document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/c0e7ae62487c201244859c735cc89d8a/invoke.js"></scr' + 'ipt>');
 
-document.write('<scr' + 'ipt type='text/javascript' src="//pl26316516.effectiveratecpm.com/67/b3/59/67b3597714ef18c324d614d4f8e57dbb.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//pl26316516.effectiveratecpm.com/67/b3/59/67b3597714ef18c324d614d4f8e57dbb.js"></scr' + 'ipt>');
